83dfd365ae85aff2a823108c748e96f923c31f78
廖文忠 <wcliao1962@gmail.com>
2021-10-04 13:34:08 +0800

    初步建立三個路由：get /, post /task, delete /task/{task}，僅/路由可串接welcome view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * Show Task Dashboard
+ */
 Route::get('/', function () {
     return view('welcome');
+});
+
+/**
+ * Add New Task
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * Delete Task
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
